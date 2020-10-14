7540071e526dd65e4d25ea60006d8322d8d5118e
Alexander Mittemueller <alexander.mittermueller@aon.at>
2020-10-14 13:42:30 +0200

    remove Node Constructor and Destructor Logs

--- Fenge/src/Parser/Nodes.h
+++ Fenge/src/Parser/Nodes.h
@@ -20,12 +20,11 @@ class LiteralNode : Node {
 public:
 	~LiteralNode() {
 		delete token;
-		LOG("~Literal");
 	}
 
 	Token* token;
 
-	LiteralNode(Token* token) : token(token) { LOG("Literal"); }
+	LiteralNode(Token* token) : token(token) { }
 
 	inline std::string toString() const override {
 		return token->toString();
@@ -38,13 +37,12 @@ public:
 	~UnaryNode() {
 		delete op;
 		delete node;
-		LOG("~Unary");
 	}
 
 	Token* op;
 	Node* node;
 
-	UnaryNode(Token* op, Node* node) : op(op), node(node) { LOG("Unary"); }
+	UnaryNode(Token* op, Node* node) : op(op), node(node) { }
 
 	inline std::string toString() const override {
 		return std::string("(") + op->toString() + node->toString() + ")";
@@ -58,14 +56,13 @@ public:
 		delete left;
 		delete op;
 		delete right;
-		LOG("~Binay");
 	}
 
 	Node* left;
 	Token* op;
 	Node* right;
 
-	BinaryNode(Node* left, Token* op, Node* right) : left(left), op(op), right(right) { LOG("Binary"); }
+	BinaryNode(Node* left, Token* op, Node* right) : left(left), op(op), right(right) { }
 
 	inline std::string toString() const override {
 		return std::string("(") + left->toString() + op->toString() + right->toString() + ")";
